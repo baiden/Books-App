88eca6c70fe10c0f2d6eecd8029975248a2afdcf
baiden <esgrey094@gmail.com>
2019-07-04 00:51:03 +0000

    Have implemented the use of the picasso library to retrieve the thumbnails of the books

--- app/src/main/java/com/example/books/ApiUtil.java
+++ app/src/main/java/com/example/books/ApiUtil.java
@@ -82,6 +82,8 @@ public class ApiUtil {
         final String ITEMS = "items";
         final String VOLUMEINFO = "volumeInfo";
         final String DESCRIPTION = "description";
+        final String IMAGELINKS = "imageLinks";
+        final String THUMBNAIL = "thumbnail";
 
         ArrayList<Book> books = new ArrayList<Book>();
         try {
@@ -93,6 +95,7 @@ public class ApiUtil {
                 JSONObject bookJSON = arrayBooks.getJSONObject(i);
                 JSONObject volumeInfoJSON =
                         bookJSON.getJSONObject(VOLUMEINFO);
+                JSONObject imageLinksJSON = volumeInfoJSON.getJSONObject(IMAGELINKS);
                 int authorNum = volumeInfoJSON.getJSONArray(AUTHORS).length();
                 String[] authors = new String[authorNum];
                 for (int j=0; j<authorNum;j++) {
@@ -105,7 +108,9 @@ public class ApiUtil {
                         authors,
                         volumeInfoJSON.getString(PUBLISHER),
                         volumeInfoJSON.getString(PUBLISHED_DATE),
-                        volumeInfoJSON.getString(DESCRIPTION));
+                        (volumeInfoJSON.isNull(DESCRIPTION)?"":volumeInfoJSON.getString(DESCRIPTION)),
+                        imageLinksJSON.getString(THUMBNAIL)
+                );
                 books.add(book);
 
             }
--- app/src/main/java/com/example/books/Book.java
+++ app/src/main/java/com/example/books/Book.java
@@ -1,8 +1,12 @@
 package com.example.books;
 
+import android.databinding.BindingAdapter;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.text.TextUtils;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class Book implements Parcelable {
     public String id;
@@ -12,9 +16,9 @@ public class Book implements Parcelable {
     public String publisher;
     public String publishedDate;
     public String description;
+    public String thumbnail;
 
-    public Book(String id, String title, String subTitle, String[] authors, String publisher, String publishedDate, String description) {
-
+    public Book(String id, String title, String subTitle, String[] authors, String publisher, String publishedDate, String description, String thumbnail) {
         this.id = id;
         this.title = title;
         this.subTitle = subTitle;
@@ -22,6 +26,7 @@ public class Book implements Parcelable {
         this.publisher = publisher;
         this.publishedDate = publishedDate;
         this.description = description;
+        this.thumbnail = thumbnail;
     }
 
     protected Book(Parcel in) {
@@ -32,6 +37,7 @@ public class Book implements Parcelable {
         publisher = in.readString();
         publishedDate = in.readString();
         description = in.readString();
+        thumbnail = in.readString();
     }
 
     public static final Creator<Book> CREATOR = new Creator<Book>() {
@@ -60,5 +66,14 @@ public class Book implements Parcelable {
         dest.writeString(publisher);
         dest.writeString(publishedDate);
         dest.writeString(description);
+        dest.writeString(thumbnail);
+    }
+
+    @BindingAdapter({"android:imageUrl"})
+    public static void loadImage (ImageView view, String imageUrl) {
+        Picasso.get()
+                .load(imageUrl)
+                .placeholder(R.drawable.book_open)
+                .into(view);
     }
 }
